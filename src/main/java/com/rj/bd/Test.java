dfa8974366a9268d1d62fcdda9da882580931488
lijie <2582381999@qq.com>
2020-04-07 16:09:23 +0800

    第一次测试提交

---
+++ src/main/java/com/rj/bd/Test.java
@@ -0,0 +1,14 @@
+package com.rj.bd;
+/**
+ * @desc    
+ * @author Lee
+ * @time    2020年4月7日
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
